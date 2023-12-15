c19ea4349270d9afd799198b2e1cb9cd24b789a0
Mnavaa <navamargareth@gmail.com>
2023-12-15 12:32:45 +0100

    estudiants3

--- estructura.py
+++ estructura.py
@@ -1,12 +1,19 @@
 #PRE El prgrama lea al usuario una lista de tuplas dónde cada tupla representa una información
 
 estudiants= ("nom", "cognom", "edat", "Nota1", "Nota2","Nota3")
-input(estudiants)
 
-nom = str(input("Cuál es tu nombre?")
-apeliido = str(input("Cuál es tu apellido?")
-edat = int(input("Cuál es tu edad?")
+
+nom = str(input("Cuál es tu nombre?"))
+apeliido = str(input("Cuál es tu apellido?"))
+edat = int(input("Cuál es tu edad?"))
 Nota1 = int(input("Cuál es tu Nota1?"))
 Nota2 = int(input("Cuál es tu Nota2?"))
 Nota3 = int(input("Cuál es tu Nota3?"))
+
+#nota1 * 0.3 
+#nota2 * 0.3
+#nota3 * 0.4
+nota_final = (Nota1 * 0.3) + (Nota2 * 0.4) + (Nota3 * 0.3)
+print(nota_final)
+
 
