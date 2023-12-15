2520b0064eed817dd006212ec72615a8cb1875ee
Albert <117260437+AlbertTeacher@users.noreply.github.com>
2023-12-15 12:15:13 +0100

    Correcciones

--- estructura.py
+++ estructura.py
@@ -1,9 +1,9 @@
 #PRE El prgrama lea al usuario una lista de tuplas dónde cada tupla representa una información
 
 estudiants= ("nom", "cognom", "edat", "Nota1", "Nota2","Nota3")
-input(estudiants)
+input(estudiants) # Aqui estas pidiendo algo por pantalla al usuario, pero no lo guardas en nungun sitio.
 
-nom = str(input("Cuál es tu nombre?")
+nom = str(input("Cuál es tu nombre?") # input ya es un string, no hace falta hacer el cast a str
 apeliido = str(input("Cuál es tu apellido?")
 edat = int(input("Cuál es tu edad?")
 Nota1 = int(input("Cuál es tu Nota1?"))
