d3c98680ffa022b8897ba9e3773224b1e4c0d44c
Mnavaa <navamargareth@gmail.com>
2023-12-15 10:58:58 +0100

    estudiants2

--- estructura.py
+++ estructura.py
@@ -3,9 +3,10 @@
 estudiants= ("nom", "cognom", "edat", "Nota1", "Nota2","Nota3")
 input(estudiants)
 
-nom = input("Cuál es tu nombre?")
-apeliido = input("Cuál es tu apellido?")
-edat = ("Cuál es tu edad?")
-Nota1 = ("Cuál es tu Nota1?")
-Nota2 = ("Cuál es tu Nota2?")
-Nota3 = ("Cuál es tu Nota3?")
+nom = str(input("Cuál es tu nombre?")
+apeliido = str(input("Cuál es tu apellido?")
+edat = int(input("Cuál es tu edad?")
+Nota1 = int(input("Cuál es tu Nota1?"))
+Nota2 = int(input("Cuál es tu Nota2?"))
+Nota3 = int(input("Cuál es tu Nota3?"))
+
