152821a759fc58a5206a37d752454d0a33867e27
marcbailo <mbailo@institutmvm.cat>
2024-01-23 14:31:54 +0100

    s

---
+++ UF2-PRO-DAW/prestec.py
@@ -0,0 +1,58 @@
+# Práctica 1
+
+# Per introduir la programació modular farem un programa per gestionar una llibreria.
+
+# Hauria de permetre afegir nous llibres, mostrar la llista de llibres i eliminar llibres.
+
+
+
+
+# Práctica 2
+
+# Hem de gestionar els préstecs de llibres. 
+
+# Aprofitant el mateix codi que hem fet a l'exercici anterior (no heu de crear un fitxer nou), i sense que perdi la funcionalitat que ja té heu de fer el següent:
+
+# Ha de permetre registrar un préstec, mostrar els llibres actualment prestats i gestionar la devolució de llibres.
+
+# Heu d'entregar el link al commit final.
+
+
+
+#Per introduir la programació modular farem un programa per gestionar una llibreria.
+#Hauria de permetre afegir nous llibres, mostrar la llista de llibres i eliminar llibres.
+
+
+
+#PRE Tenemos que crear una libreria que nos permita añadir libros 
+
+añadir = 1
+eliminar = 2
+mostrar = 3
+
+opciones_libreria = input ("Pulsa 1 para añadir, pulsa 2 para eliminar y pulsa 3 para mostrar")
+
+
+if funcion == añadir 
+libro = input ("Añade el libro seleccionado")
+print(libro)
+
+
+
+if funcion == eliminar 
+libro = input ("Elimina el libro seleccionado")
+print(libro)
+
+if funcion == mostrar 
+libro = input ("Muestra el libro seleccinado")
+print(libro)
+
+funcion = True
+
+
+
+#POST Nos permite añadir nuevos libros, mostrarlos y borrarlos
+
+
+
+
