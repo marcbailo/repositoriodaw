67ed6eef66464d0f8c6c27f4c687b3bf2c7afc4d
marcbailo <151924774+marcbailo@users.noreply.github.com>
2024-01-23 13:39:11 +0100

    Update libreria.py
    No me dejaba hacer un commit, así que te he editado el commit anterior.

--- UF2-PRO-DAW/libreria.py
+++ UF2-PRO-DAW/libreria.py
@@ -8,14 +8,23 @@ añadir = 1
 eliminar = 2
 mostrar = 3
 
+opciones_libreria = input ("Pulsa 1 para añadir, pulsa 2 para eliminar y pulsa 3 para mostrar")
 
-if funcion == añadir
+
+if funcion == añadir 
 libro = input ("Añade el libro seleccionado")
 print(libro)
 
-if funcion == eliminar
+if funcion == eliminar 
 libro = input ("Elimina el libro seleccionado")
 print(libro)
 
-if funcion == mostrar
-libro = input ("Muestra el libro seleccioando"
+if funcion == mostrar 
+libro = input ("Muestra el libro seleccinado")
+print(libro)
+
+funcion = True
+
+
+
+#POST Nos permite añadir nuevos libros, mostrarlos y borrarlos
