94393d39372be78febfba9e3d7c185ad284ef5c6
marcbailo <mbailo@institutmvm.cat>
2024-01-23 13:31:41 +0100

    commit final libreria

--- UF2-PRO-DAW/libreria.py
+++ UF2-PRO-DAW/libreria.py
@@ -19,5 +19,12 @@ if funcion == eliminar
 libro = input ("Elimina el libro seleccionado")
 print(libro)
 
-if funcion == mostrar
-libro = input ("Muestra el libro seleccinado"
+if funcion == mostrar 
+libro = input ("Muestra el libro seleccinado")
+print(libro)
+
+funcion = True
+
+
+
+#POST Nos permite añadir nuevos libros, mostrarlos y borrarlos
