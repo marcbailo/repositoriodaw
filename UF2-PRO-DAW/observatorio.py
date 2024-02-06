8ca2d0b46701802a4f136c6788f466cdda639209
marcbailo <mbailo@institutmvm.cat>
2024-02-06 13:52:36 +0100

    primer commit observatorio

---
+++ UF2-PRO-DAW/observatorio.py
@@ -0,0 +1,13 @@
+#PRE
+# Hay que crear un código basado en el documento hecho anteiormente, este código tiene que registrar y contener las temperaturas semanales de todo el año.
+
+
+temperatuta = []
+
+
+def registro_Fecha():
+    fecha_semanal = input ("Devuelveme la fecha de cada lunes semanal de todo el año, empezando por el 1 de enero")
+
+def registro_temperatura_semanal():
+    temperatura_semanal = input ("Devuelveme la temperatura de cada semana de todo el año, empezando por el 1 de enero")    
+
