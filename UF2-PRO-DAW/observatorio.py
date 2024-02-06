51e9b69805ed700f558dd99c815127b8042a5d7d
marcbailo <mbailo@institutmvm.cat>
2024-02-06 14:25:14 +0100

    h

--- UF2-PRO-DAW/observatorio.py
+++ UF2-PRO-DAW/observatorio.py
@@ -2,12 +2,31 @@
 # Hay que crear un código basado en el documento hecho anteiormente, este código tiene que registrar y contener las temperaturas semanales de todo el año.
 
 
-temperatuta = []
+temperatura = []
 
+semana = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52]
 
 def registro_Fecha():
     fecha_semanal = input ("Devuelveme la fecha de cada lunes semanal de todo el año, empezando por el 1 de enero")
+    return(semana)
+
 
 def registro_temperatura_semanal():
-    temperatura_semanal = input ("Devuelveme la temperatura de cada semana de todo el año, empezando por el 1 de enero")    
+    temperatura_semanal = input ("Devuelveme la temperatura de cada semana de todo el año, empezando por el 1 de enero") 
+    return(temperatura_semanal)
+
+def registro_temperatura_max ():
+    temperatura_max = input ("Devuelveme la temperatura más alta de entre todas ")
+    return (temperatura_max)
+
+
+def registro_temperatura_min ():
+    temperatura_min = input ("Devuelveme la temperatura más alta de entre todas ")
+    return (registro_temperatura_min)
+
+    
+    
+    
+    
+
 
