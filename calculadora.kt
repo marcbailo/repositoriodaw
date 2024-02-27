0419f521080b32026b5662ca23f748ce7e1ef567
marcbailo <mbailo@institutmvm.cat>
2024-02-27 13:18:29 +0100

    he puesto los paréntesis

--- calculadora.kt
+++ calculadora.kt
@@ -1,25 +1,25 @@
 
 
 fun sumar(a: Int, b: Int): Int {
-    return (a + b
+    return (a + b)
 }
 
 println(sumar)
 
 
 fun restar(a: Int, b: Int): Int {
-    return a - b
+    return (a - b)
 }
 println(restar)
 
 fun multiplicar(a: Int, b: Int): Int {
-    return a * b
+    return (a * b)
 }
 println(multiplicar)
 
 
 fun dividir(a: Int, b: Int): Int {
-    return a / b
+    return (a / b)
 }
 println(dividir)
 
