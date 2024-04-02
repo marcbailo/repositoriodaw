79a88fd7d32d13a903632845ba293d05d480aec4
marcbailo <mbailo@institutmvm.cat>
2024-04-02 14:03:40 +0200

    UF3 documento texto

---
+++ kotlin/viajesreal.kt
@@ -0,0 +1,67 @@
+#Requisits:
+
+#1. Gestió de Viatges:
+   - Poder afegir un nou viatge a la llista.
+   - Poder obtenir informació resumida de tots els viatges.
+   - Donar informació detallada d'un viatge específic.
+
+Gestión de viajes
+   1.1 - Añadir viaje seleccionado
+   1.2 - Resumir los viajes
+   1.3 - Que nos devuelva la información resumida
+   1.4 - Detallar información por viaje
+   1.5 - Que nos devuelva la información detallada
+
+#2. Client i Reserva:
+   - Registrar un nou client i obtenir un identificador únic.
+   - Permetre a un client fer una reserva per a una destinació específica.
+   - Donar informació detallada d'una reserva.
+
+   2.1 - Registramos un cliente
+   2.2 - Añdadir un indetificador unico al cliente
+
+
+
+
+3. Interfície d'Usuari:
+   - Mostrar les opcions disponibles a l'usuari.
+   - Executar l'acció associada a l'opció seleccionada per l'usuari.
+
+
+
+
+lista_usuarios()
+lista_viajes()
+
+
+
+fun mostrarMenu() {
+
+    println("--- Menu ---")
+    println("1. Afegiu un viatge")
+    println("2. Consultar viatges")
+    println("3. Detallsviatge")
+    println("4. Registrar client")
+    println("5. Fer reserva")
+    println("6 ")
+
+}
+
+
+fun leerOpcio(): Int {
+    print("Selecciona una opcio: ")
+    va opcio:Int = readLine()?.toInt()
+
+   
+}
+
+
+
+
+
+
+ fun main() {
+    mostrarMenu()
+        val optio = leerOpcio()
+        // Executar opcio
+    }
--- mar/viajes.kt
+++ mar/viajes.kt
@@ -4,24 +4,48 @@
 
 // # 1. Gestió de Viatges:
 // #    - Poder afegir un nou viatge a la llista.
+    fun main(arg:Array<String>){
+    var listaViajes: List<String> = listOf("idk, idk, idk")
+    println("Lista: $listaViajes")
+    }
 // #    - Poder obtenir informació resumida de tots els viatges.
 // #    - Donar informació detallada d'un viatge específic.
 
 // fun gestiondeViajes(arg:Array<String>){
 //     var myEmptyList: List<Int> = listof()
 //     prin
+// }
+fun gestiondeViajes(): String{
+    println("Benvenido a la aerolinia Mimis")
+    println("1.Gestion de Viajes")
+    println("2.Clientes y reservas")
+    println("3.a")
+    var viajitos: String? = readLine()
+
+	if (viajitos == null) {
+		viajitos = ""
+	}
+
+	return viajitos
+}
+
+
 // # 2. Client i Reserva:
 // #    - Registrar un nou client i obtenir un identificador únic.
 // #    - Permetre a un client fer una reserva per a una destinació específica.
 // #    - Donar informació detallada d'una reserva.
-// fun client():
+fun client():
 
 // fun reserva():
 // # 3. Interfície d'Usuari:
 // #    - Mostrar les opcions disponibles a l'usuari.
 // #    - Executar l'acció associada a l'opció seleccionada per l'usuari.
-// fun usuari():
+ fun usuari():
 // # Nota:
 // # Pots assumir que ja tens una llista de viatges, clients i reserves inicials per a les primeres proves de l'aplicació.
 
 
+SELECT e.employee_name, d.department_name, p.project_name
+    FROM employees e
+    LEFT JOIN departments d ON e.department_id = d.department_id
+    LEFT JOIN projects p ON e.project_id = p.project_id;
