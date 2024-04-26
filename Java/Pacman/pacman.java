021cdbb71085eda63e1c1f4fa933be3e088772b3
marcbailo <mbailo@institutmvm.cat>
2024-04-26 12:01:30 +0200

    hi

--- Java/Pacman/pacman.java
+++ Java/Pacman/pacman.java
@@ -12,6 +12,7 @@ private int Poder;
 
 }
 
+// Valores añadidos a Pacman que devuelven como la posición, vidas...
 
 public Pacman(int initialnewPosX, int initialnewPosY) {
 
@@ -23,6 +24,38 @@ this.poder = false;
 
 
 }
+
+ // La estructura del movimiento de Pacman, cómo se mueve hacia arriba, abajo, derecha e izquierda
+
+public void move(int newPosX, int newPosY) {
+
+public moveup(int moveY) {
+
+this.moveY += 1;
+
+}
+
+public movedown(int moveY) {
+
+this.moveY -=1;
+
+}
+
+public moveleft(int moveX) {
+
+this.moveX += 1;   
+
+} 
+
+public moveright(intmoveX) {
+
+this.moveX -=1;
+
+}
+
+
+
+
 
 
 
