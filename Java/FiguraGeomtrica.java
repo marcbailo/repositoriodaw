d97017f6dab2ad34f527350df2a48c3c78217100
marcbailo <mbailo@institutmvm.cat>
2024-04-30 13:09:58 +0200

    hago pública la interface y añado void a los métodos.

--- Java/FiguraGeomtrica.java
+++ Java/FiguraGeomtrica.java
@@ -1,10 +1,16 @@
  interface FiguraGeometrica {
 
-public abstract calcularArea()
-public abstract calcularPerimetre()
+public abstract calcularArea();
+public abstract calcularPerimetre();
 
 
 
 }
 
+public interface FiguraGeometrica {
 
+
+public void calcularArea();
+public void calcularPerimetre();
+
+}
