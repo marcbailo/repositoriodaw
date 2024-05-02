db5041a9c77ef2fe0c448789bc8d633517f7fc36
marcbailo <mbailo@institutmvm.cat>
2024-05-02 11:00:42 +0200

    primer commit

---
+++ Java/Notas_Alumnos/RegistroNotas.java
@@ -0,0 +1,14 @@
+public class GestorNotes {
+
+
+
+}
+
+class registrarNotes {
+
+public GestorNotes(String numAlumne, double[] notes) {
+    this.numAlumne = numAlumne;
+    this.notes = notes;
+   
+    }
+}
