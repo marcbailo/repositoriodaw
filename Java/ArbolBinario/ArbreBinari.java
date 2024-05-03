63facb2a7da19f9fa69512968bfd7eafdb21e244
marcbailo <mbailo@institutmvm.cat>
2024-05-03 11:42:44 +0200

    Primer commit, he creado la clase Arbol  con los datos int.

---
+++ Java/ArbolBinario/ArbreBinari.java
@@ -0,0 +1,39 @@
+
+//1. Crea una nova classe en Java per a la implementació de l'arbre binari.
+
+public class ArbreBinari {
+
+    int left;
+    int right;
+    int data;
+     
+}
+
+//2. Implementa constructors per a la creació de l'arbre binari.
+
+
+//3. Volem que les instancies d’arbre sapiguen quantes instancies coexisteixen a la vegada.
+
+
+//4. Volem poder fer servir dos tipus d’arbres, un d’enters i un altre de caracters.
+
+//5. No volem que hi hagi cap tipus d’ordenació, simplement volem que estigui balancejat, aixó vol dir que cada branca tingui la mateixa profunditat.
+
+//6. Crear una classe Principal que s’encarregui de fer les següents tasques:
+
+
+
+
+
+//A. Crear 2 Arbres d’enters de fondaria 5.
+//B. Mostrar quantes instancies existeixen d‘arbres.
+//C. Crear 2 Arbres de caràcters de fondaria 3.
+//D. Mostrar quantes instancies existeixen d’arbres.
+//E. Forçar al Garbage Collector.
+//F. Recorrer el primer arbre d’enters i mostrant els valors per pantalla, fent servir unica i exclusivament les branques dretes.
+//G. Recorrer el segon arbre d’enters i mostrant els valors per pantalla, fent servir unica i exclusivament les branques esquerres.
+//H. Tornar a mostrar quantes instancies existeixen.
+//I. Tornar a forçar al Garbage Collector.
+//J. Tornar a mostrar quantes instancies existeixen.
+//7. Opcional: Podries fer alguna forma de saber per pantalla que ha sigut eliminat per el Garbage Collector? Fes-ho.
+
--- Java/Notas_Alumnos/RegistroNotas.java
+++ Java/Notas_Alumnos/RegistroNotas.java
@@ -12,3 +12,49 @@ public GestorNotes(String numAlumne, double[] notes) {
    
     }
 }
+
+
+
+
+//CORRECCION
+
+
+public class GestorNotes
+{
+    public void registrarNotes(String nomAlumne, double[] notes) {
+
+    }
+
+    public double[] obtenirNotes(String nomAlumne) {
+        return new double[]{0,0};
+    }
+}
+
+
+
+import java.util.Hastable;
+
+public class GestorNotes
+{
+
+    private Hashtable<String, double[]> alumnes = new Hashtable<String, double[]>();
+
+    public void registrarNotes(String nomAlumne, double[] notes) {
+        alumnes.put(nomAlumne, notes);
+
+        public double[] obtenirNotes(String nomAlumne) {
+            return alumnes.get(nomAlumne);
+        }
+    }
+
+
+
+
+public class CalculadoraEstadisitques
+{
+
+public double calcularMitjana(double[] notes) {
+    double suma = 0.0;
+}
+
+}
