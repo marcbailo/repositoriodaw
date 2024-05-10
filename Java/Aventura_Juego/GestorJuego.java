102d69d94f7d47314ac2429a3cab8b5b3ee7dbfd
marcbailo <mbailo@institutmvm.cat>
2024-05-10 12:19:31 +0200

    Creamos la clase GestorJuego. En ella implementamos los métodos para cargar los personajes desde xml, añadimos return null a cada método para evitar errores de compilación.

--- Java/Aventura_Juego/GestorJuego.java
+++ Java/Aventura_Juego/GestorJuego.java
@@ -1,1 +1,28 @@
+import java.util.ArrayList;
 
+public class GestorJuego {
+
+    // Método para cargar personajes desde XML
+    public static ArrayList<Personaje> cargarPersonajesDesdeXML(String personatges.XML) {
+       
+        
+        
+        return null; // para evitar errores de compilación
+    }
+
+    // Método para cargar armas desde XML
+    public static ArrayList<Arma> cargarArmasDesdeXML(String personatges.XML) {
+        
+    
+       
+        return null; // para evitar errores de compilación
+    }
+
+    // Método para cargar armaduras desde XML
+    public static ArrayList<Armadura> cargarArmadurasDesdeXML(String personatges.XML) {
+       
+        
+       
+        return null; // para evitar errores de compilación
+    }
+}
