3496aa006e0ffdbb94d3a6e006352ba5464b8b64
marcbailo <mbailo@institutmvm.cat>
2024-05-10 12:08:49 +0200

    Hacemos lo mismo con la clase armadura, añadimos constructor y el return.

--- Java/Aventura_Juego/armadura.java
+++ Java/Aventura_Juego/armadura.java
@@ -1,6 +1,13 @@
 public class Armadura {
     private String nom;
 
-   
-}
+    // Constructor
+    public Armadura(String nom) {
+        this.nom = nom;
+    }
 
+    // Return
+    public String getNom() {
+        return nom;
+    }
+}
