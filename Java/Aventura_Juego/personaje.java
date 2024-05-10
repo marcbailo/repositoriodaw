5fbd244652fd76fc844ae214f3f30695cc8bc962
marcbailo <mbailo@institutmvm.cat>
2024-05-10 12:05:58 +0200

    Creamos los return de los atributos del personaje para que devuelva el valor.

--- Java/Aventura_Juego/personaje.java
+++ Java/Aventura_Juego/personaje.java
@@ -16,5 +16,37 @@ public class Personaje {
         this.armadura = armadura;
     }
 
-    
+    // Return
+    public String getNom() {
+        return nom;
+    }
+
+    public int getNivell() {
+        return nivell;
+    }
+
+    public int getPuntsDeVida() {
+        return puntsDeVida;
+    }
+
+    public int getPuntsDeMana() {
+        return puntsDeMana;
+    }
+
+    public Arma getArma() {
+        return arma;
+    }
+
+    public void setArma(Arma arma) {
+        this.arma = arma;
+    }
+
+    public Armadura getArmadura() {
+        return armadura;
+    }
+
+    public void setArmadura(Armadura armadura) {
+        this.armadura = armadura;
+    }
+}
 
