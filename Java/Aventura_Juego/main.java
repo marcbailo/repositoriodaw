f0dbce09a7ed6d5b98ae0b9969c61a444e37d5c3
marcbailo <mbailo@institutmvm.cat>
2024-05-10 12:25:14 +0200

    Creamos la clase main, hacemos el try catch para ver si encuentra errores.

---
+++ Java/Aventura_Juego/main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Cargar personajes, armas y armaduras desde XML
+            ArrayList<Personaje> personajes = GestorJuego.cargarPersonajesDesdeXML("personatjes.xml");
+            ArrayList<Arma> armas = GestorJuego.cargarArmasDesdeXML("arma.xml");
+            ArrayList<Armadura> armaduras = GestorJuego.cargarArmadurasDesdeXML("armadura.xml");
+            
+            // Realizar otras operaciones con los personajes, armas y armaduras cargados
+        } catch (Exception e) {
+            // Manejo de excepciones (p.ej., archivo no encontrado)
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
