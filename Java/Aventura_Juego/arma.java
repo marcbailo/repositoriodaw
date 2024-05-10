a540967ed949ebab97c7aeab577bfe342a982167
marcbailo <mbailo@institutmvm.cat>
2024-05-10 12:07:40 +0200

    Añadimos constructor y return a la clase arma.

--- Java/Aventura_Juego/arma.java
+++ Java/Aventura_Juego/arma.java
@@ -1,2 +1,13 @@
 public class Arma {
     private String nom;
+
+    // Constructor
+    public Arma(String nom) {
+        this.nom = nom;
+    }
+
+    // Return
+    public String getNom() {
+        return nom;
+    }
+}
