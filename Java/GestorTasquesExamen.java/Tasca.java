dd486bc474c1e265e2f1daeb918fd2e92354db5e
marcbailo <mbailo@institutmvm.cat>
2024-05-17 11:47:43 +0200

    He hecho los get y los set para acceder a los campos de los atributos, con los return para que devuelvan el resultado.

--- Java/GestorTasquesExamen.java/Tasca.java
+++ Java/GestorTasquesExamen.java/Tasca.java
@@ -18,6 +18,50 @@ public class Tasca {
     }
 
 
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
 
 
 
