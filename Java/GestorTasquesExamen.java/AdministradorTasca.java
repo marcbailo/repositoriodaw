90a72fd20a6f6b41e8a95e5614d354024b3cc06b
marcbailo <mbailo@institutmvm.cat>
2024-05-17 12:23:31 +0200

    He puesto los 3 últimos imports. Java.nio.file para manipular archivos y directorios y java.util para estructurar datos y utilidades, como arrays o listas. También he puesto el java.time.LocalDate, es el mismo que puse en tasca.java.

--- Java/GestorTasquesExamen.java/AdministradorTasca.java
+++ Java/GestorTasquesExamen.java/AdministradorTasca.java
@@ -1,4 +1,8 @@
 import java.io.*;
+import java.nio.file.*;
+import java.time.LocalDate;
+import java.util.*;
+
 
 public class AdministradorTasca {
     private Map<String, List<Task>> priorityLists; // Mapa per a llistes de tasques per prioritat.
