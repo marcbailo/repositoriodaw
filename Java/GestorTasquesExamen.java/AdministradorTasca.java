495e4e28b5666912c57d304bb4ce454a643cfbd1
marcbailo <mbailo@institutmvm.cat>
2024-05-17 12:19:14 +0200

    En la nueva clase que he creado he añadido 3 atributos privados, priorityLists sirve para priorizar las listas, taskmap para para identificar las tareas por su id en un mapa y filename para ver el nombre del fichero donde se guardan las tareas.

--- Java/GestorTasquesExamen.java/AdministradorTasca.java
+++ Java/GestorTasquesExamen.java/AdministradorTasca.java
@@ -1,10 +1,8 @@
 import java.io.*;
 
-
 public class AdministradorTasca {
+    private Map<String, List<Task>> priorityLists; // Mapa per a llistes de tasques per prioritat.
+    private Map<Integer, Task> taskMap; // Mapa per a tasques identificades pel seu ID.
+    private String filename; // Nom del fitxer on es guarden les tasques.
 
-
-
-
-
-}
+   
