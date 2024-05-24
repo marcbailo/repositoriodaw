e6ab1a9503990a29fe8cce9da19e4af347b0e00b
marcbailo <151924774+marcbailo@users.noreply.github.com>
2024-05-24 11:55:00 +0200

    Update robot.java
    Definimos la clase robot y le añadimos la extensión JFrame, esto hará que la clase sea la ventana de la interfaz gráfica dónde veremos los botones. 
    
    Luego he declarado los componentes de la interfaz con un JLabel para mostrar el estado, los botones del robot lo he hecho con un JButton para meter sus movimientos.

--- Java/RObot/robot.java
+++ Java/RObot/robot.java
@@ -1,3 +1,13 @@
-private class robot {
-    
+public class Robot extends JFrame {
+
+private JLabel estadoLabel;
+private JButton moverEndavantButton;
+private JButton moverEnrereButton;
+private JButton girarEsquerraButton;
+private JButton girarDretaButton;
+private JButton guardarEstatButton;
+private JButton carregarEstatButton;
+
+
+
 }
