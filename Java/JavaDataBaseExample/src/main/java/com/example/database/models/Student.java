06f6c23ad62181dd957ccd85cb9413d74984efc7
marcbailo <mbailo@institutmvm.cat>
2024-05-28 13:38:41 +0200

    creamos la clase Student, le añadimos un constructor y unos parámetros

---
+++ Java/JavaDataBaseExample/src/main/java/com/example/database/models/Student.java
@@ -0,0 +1,15 @@
+package com.example.database.models;
+
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Getters y setters
+}
