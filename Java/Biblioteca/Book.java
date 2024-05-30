6ea77d5f92c3be898d19eaf5e8c59648e547b182
marcbailo <mbailo@institutmvm.cat>
2024-05-30 10:38:40 +0200

    método para acceder y modificar los atributos de los libros

--- Java/Biblioteca/Book.java
+++ Java/Biblioteca/Book.java
@@ -19,11 +19,14 @@ public void info() {
     }
 
 
-
-
-
-
-
-
-
+public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public int getAvailableCopies() { return availableCopies; }
+    public void setAvailableCopies(int availableCopies) { this.availableCopies = availableCopies; }
 }
+
+
+
+
+
+
