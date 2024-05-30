984b8623468bb2ecf6bb355b6a60c548261247af
marcbailo <mbailo@institutmvm.cat>
2024-05-30 10:57:18 +0200

    creamos la clase App y añadimos 2 libros.

---
+++ Java/Biblioteca/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public void exec() {
+        
+        Book book1 = new Book("LOTR", "J.R.R. Tolkien", 5);
+        Book book2 = new Book("Mistborn", "Brandon Sanderson", 3);
+
+    }
+
+}       
