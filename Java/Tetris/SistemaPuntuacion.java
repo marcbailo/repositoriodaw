9bef7a70ffc4ba7e60b315deeaf5367fc59b7433
marcbailo <mbailo@institutmvm.cat>
2024-05-31 12:26:14 +0200

    agregamos atributos para definir la puntuación máxima o mínima, también agregamos la puntuación del juego y un reset para cuando se finalize.

--- Java/Tetris/SistemaPuntuacion.java
+++ Java/Tetris/SistemaPuntuacion.java
@@ -3,3 +3,13 @@ public class SistemaPuntuacion {
     int puntuacionMax;
     int score;
 
+public SistemaPuntuacion(int puntuacionMin, int puntuacionMax) {
+        this.puntuacionMin = puntuacionMin;
+        this.puntuacionMax = puntuacionMax;
+        this.score = 0;
+    }
+
+    public void reset() {
+        score = 0;
+    }
+}
