cfc203033410176efddd624249acbc8b9ae32f2a
marcbailo <mbailo@institutmvm.cat>
2024-05-31 12:28:29 +0200

    nueva clase, he agregado inte y bollean, no tengo tiemp me queda un minuto

---
+++ Java/Tetris/Temporizador.java
@@ -0,0 +1,8 @@
+public class Temporizador {
+    int intervalo;
+    boolean running;
+
+    public Temporizador(int intervalo) {
+        this.intervalo = intervalo;
+        this.running = false;
+    }
