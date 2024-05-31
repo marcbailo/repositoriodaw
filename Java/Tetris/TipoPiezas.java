348e703923d8a0030d5b16a8d0ea29011d395783
marcbailo <mbailo@institutmvm.cat>
2024-05-31 12:18:32 +0200

    Añado nueva clase y pongo todas las piezas y sus caracterísitcas como el color o su posición. También el tipo de letra que representa la pieza.

---
+++ Java/Tetris/TipoPiezas.java
@@ -0,0 +1,9 @@
+public class TipoPiezas {
+    public static Piezas piezaCyan = new Piezas("Pieza I", "cyan", new Position(0, 0));
+    public static Piezas piezaYellow = new Piezas("Pieza O", "yellow", new Position(0, 0));
+    public static Piezas piezaBlue = new Piezas("Pieza J", "blue", new Position(0, 0));
+    public static Piezas piezaOrange = new Piezas("Pieza L", "orange", new Position(0, 0));
+    public static Piezas piezaGreen = new Piezas("Pieza S", "green", new Position(0, 0));
+    public static Piezas piezaRed = new Piezas("Pieza Z", "red", new Position(0, 0));
+    public static Piezas piezaPurple = new Piezas("Pieza T", "purple", new Position(0, 0));
+}
