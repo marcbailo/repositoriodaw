3d2a3dfba93f0252b880a1c4ff9d3224d05afb3b
marcbailo <mbailo@institutmvm.cat>
2024-05-31 12:11:21 +0200

    iniciamos el constructor con sus respectivos atributos

--- Java/Tetris/Piezas.java
+++ Java/Tetris/Piezas.java
@@ -2,3 +2,14 @@ public class Piezas {
     String nombre;
     String color;
     Position position;
+
+ public Piezas(String nombre, String color, Position position) {
+        this.nombre = nombre;
+        this.color = color;
+        this.position = position;
+    }
+
+
+
+    
+}    
